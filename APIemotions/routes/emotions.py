892b77d32c13a07cab73f698988516f959ecc95f
paula.fuentes <paula.fuentes@territoriumlife.com>
2021-04-19 08:32:47 -0500

    change file type

--- APIemotions/routes/emotions.py
+++ APIemotions/routes/emotions.py
@@ -5,18 +5,28 @@ import cv2
 import sys
 import json
 import os
+import base64
+import numpy as np
 
 
-def checkEmotions(path):
+def Base64(code64):
+    image = code64
+    decoded_data = base64.b64decode(image)
+    np_data = np.fromstring(decoded_data,np.uint8)
+    img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
+    return img
+
+
+def checkEmotions(img):
     ''' detect an emotions from a face '''
 
-    img = cv2.imread(path)
     detector = FER()
     detector.detect_emotions(img)
-    emotion, score = detector.top_emotion(img) # 'happy', 0.99
+    emotion, score = detector.top_emotion(img)
 
     return emotion, score
 
+'''
 #path = sys.argv[1]
 #PATH = "emotions/" + path
 path = os.path.abspath("routes")
@@ -24,5 +34,11 @@ PATH = path + "/emotions/" + sys.argv[1]
 #print(path)
 emotion, score = checkEmotions(PATH)
 #emotion, score = checkEmotions(path)
+'''
+
+code64 = sys.argv[1]
+img = Base64(code64)
+emotion, score = checkEmotions(img)
+
 print(json.dumps({"emotion": emotion, "score": score}))
 sys.stdout.flush()
