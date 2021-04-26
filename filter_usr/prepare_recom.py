f74062ce41c3385388d9c7e6dd5c74157c138296
cristian gomez <1154@holbertonschool.com>
2021-04-26 12:30:09 -0400

    [add] functions documentation

--- filter_usr/prepare_recom.py
+++ filter_usr/prepare_recom.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-""""""
+"""module that prepares the songs to recommend"""
 import pandas as pd
 import random
 
 
 def search_song(song_list, person_songs_keys):
-    """"""
+    """function that finds cations given a list of keys
+
+    Args:
+    -> song_list the complete list of songs
+    -> person_songs_keys the keys to find in the song list
+
+    Returns:
+    -> the song list updated
+    """
     return song_list.loc[person_songs_keys]
 
 
 def prepare_songs(keys):
-    """"""
+    """function that organizes the song list
+
+    Args:
+    -> keys the name of the songs to be cleaned in the list
+
+    Returns:
+    -> a list of songs
+    """
     path = './data_emotions/list-songs.csv'
     song_list = pd.read_csv(path, delimiter=',')
     song_list = song_list.set_index('Song Name')
@@ -21,7 +36,16 @@ def prepare_songs(keys):
 
 
 def music_recommender(data, p_filter, p_keys):
-    """"""
+    """function that finds the songs to recommend
+
+    Args:
+    -> data the complete data that contains the users
+    -> p_filter the name of the user to find in the data
+    -> p_keys the list of songs to clean the data
+
+    Returns:
+    -> A dict with the recommended song
+    """
     song = {}
     person = data.loc[p_filter]
     person = person.drop(['Name', 'Emotion'])
